0eba2687fdf91e9fa8031ba07af196df9a01fb58
Edwin Dominguez <edwindominguez@hotmail.com>
2020-09-12 17:31:53 -0500

    initial commit

---
+++ src/test/java/com/urbanet/platform/UrbanetPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbanet.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanetPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
